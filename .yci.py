26d8919f6a38c0e4bb935a06f50b23b09aa84eeb
jpic <jamespic@gmail.com>
2015-07-20 22:18:11 +0200

    test.sh path

--- .yci.py
+++ .yci.py
@@ -5,5 +5,5 @@ def test_generator(build_id, pull):
     for x in ENV_VAR_1:
         for y in ENV_VAR_2:
             # there i can skip or implement basic logic
-            yield {'CMD': 'test.sh', 'ENV_VAR_1': x, 'ENV_VAR_2': y,
+            yield {'CMD': './test.sh', 'ENV_VAR_1': x, 'ENV_VAR_2': y,
                    'RETRIES': 3, 'RETRY_AFTER': 600}
